3f33439b4b608cba7f492530ac0439bf03122e0a
Noura Hassan <54744761+NouraHassan12@users.noreply.github.com>
2020-07-05 22:26:43 -0700

    update2

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from "react-router-dom"
+import { BrowserRouter, Route } from "react-router-dom"
 import App from './App';
 import Imageview from './Components/Imageview'
 
 export default function AppRouter() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Switch>
+   
       <Route exact path="/" component={App}  />
       <Route path="/image/:id" component={Imageview} />
-    </Switch>
+
   </BrowserRouter>
   );
 }
